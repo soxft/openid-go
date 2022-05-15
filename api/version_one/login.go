803c0f0f145553f6529c29643da840b13dd59149
soxft <email@xcsoft.top>
2022-05-15 14:36:31 +0800

    fix: bug

--- api/version_one/login.go
+++ api/version_one/login.go
@@ -1,10 +1,20 @@
 package version_one
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"openid/library/apiutil"
+)
 
 // Login
 // @description v1 登录
 // @route GET /v1/login
 func Login(c *gin.Context) {
-	c.Redirect(302, "/login?"+c.Request.URL.Query().Encode())
+	api := apiutil.New(c)
+	appid := c.DefaultQuery("appid", "")
+	redirectUri := c.DefaultQuery("redirect_uri", "")
+	if appid == "" || redirectUri == "" {
+		api.Fail("错误的参数")
+		return
+	}
+	c.Redirect(302, "/v1/"+appid+"?redirect_uri="+redirectUri)
 }
